425e13050963d96ec38b7f41ccb7ad50483c81dc
bbanderson <tombyun@naver.com>
2021-08-27 10:21:41 +0900

    Add hooks

--- src/containers/CounterContainer.jsx
+++ src/containers/CounterContainer.jsx
@@ -1,23 +1,16 @@
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const { number } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
--- src/containers/TodosContainer.jsx
+++ src/containers/TodosContainer.jsx
@@ -1,28 +1,28 @@
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 
-const TodosContainer = ({
-  inputText,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { inputText, todos } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (inputText) => dispatch(changeInput(inputText)),
+    [dispatch],
+  );
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
   return (
     <Todos
       inputText={inputText}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({ inputText: todos.inputText, todos: todos.todos }),
-  { changeInput, insert, toggle, remove },
-)(TodosContainer);
+export default TodosContainer;
